7dcccb791fc97badaa73063272dc1641dc1c599b
konaki <konaki@anokonaki.org>
2026-07-27 09:46:11 +0800

    upgrade minmal.c example

--- examples/minimal.c
+++ examples/minimal.c
@@ -6,6 +6,7 @@
 #include "konwinlib.h"
 
 #define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
 
 #define WINDOW_TITLE "konwinlib - test"
 #define WINDOW_WIDTH 640
@@ -15,8 +16,23 @@
 
 int main(void) {
 
-	kon_init();
+	if (!kon_init()) {
+		fprintf(stderr, "kon_init: error initializing...\n");
+		return EXIT_FAILURE;
+	}
 
+	kon_window_t *window = kon_createWindow(WINDOW_TITLE, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+	if (!window) {
+		fprintf(stderr, "kon_createWindow: error creating window...\n");
+		kon_deinit();
+		return EXIT_FAILURE;
+	}
+
+	puts("tested!");
+
+	/*** cleanup ***/
+
+	kon_destroyWindow(window);
 	kon_deinit();
 
 	return EXIT_SUCCESS;
