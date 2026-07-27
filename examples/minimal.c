a125250c163189355a1b5aa8624551a784ac2942
konaki <konaki@anokonaki.org>
2026-07-27 08:59:14 +0800

    add minimal.c example

---
+++ examples/minimal.c
@@ -0,0 +1,19 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+
+#define KONWINLIB_IMPLEMENTATION
+#include "konwinlib.h"
+
+#define EXIT_SUCCESS 0
+
+#define WINDOW_TITLE "konwinlib - test"
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+#define FPS 60
+
+int main(void) {
+
+	return EXIT_SUCCESS;
+}
